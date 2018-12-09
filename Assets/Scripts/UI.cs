537ae26d2630b5d5c19a6afeed39c853b211217f
Punit Kunjam <punit17100@iiitnr.edu.in>
2018-12-09 19:33:45 +0530

    update

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+	// Use this for initialization
+    public Text ScoreText;
+    public Text timeText;
+    public int score;
+    public int time;
+
+	void Start ()
+	{
+	    score = 0;
+	    time = 0;
+	    InvokeRepeating("updateScore", 1.0f, 0.5f);
+       
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    ScoreText.text = "Score: " + score;
+	    timeText.text = "Time: " + time;
+
+	    if (time == 120)
+	    {
+	        Time.timeScale = 0;
+	    }
+        
+	}
+
+    void updateScore()
+    {
+        score += 3;
+        time += 1;
+    }
+    
+}
---
+++ Assets/Scripts/carController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class carController : MonoBehaviour {
+
+	// Car controls
+    float carSpeed = 10;
+    float maxPos = 2.5f;
+    Vector3 position;
+
+    
+    
+	void Start ()
+	{
+	    position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime; // Time.deltaTime -> time took to render the last frame
+	    position.x = Mathf.Clamp(position.x, -2.5f, 2.5f); // clamps the position of car 
+	    transform.position = position;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("obstacle"))
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
